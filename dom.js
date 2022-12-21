7c8948f04fc24981f0978066de81b6e34a63e7bd
Mpranab-123 <pranabk.mishra05@gmail.com>
2022-12-21 23:51:41 +0530

    Get element by class name

--- dom.js
+++ dom.js
@@ -26,3 +26,13 @@ console.log(main);
 console.log(main[0]);
 main[0].style.fontWeight='bold';
 main[0].style.color='green';
+
+//get element by class name:
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor= 'green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+    items[i].style.color='red'
+}
